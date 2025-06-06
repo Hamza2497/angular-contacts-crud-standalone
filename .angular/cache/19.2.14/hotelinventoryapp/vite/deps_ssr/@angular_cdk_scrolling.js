e21830afdc622637b77c9d245187f07ce06a8f05
Hamza <hamzahassaf08@gmail.com>
2025-06-06 23:24:34 +0300

    Add .DS_Store files to src and src/app directories

---
+++ .angular/cache/19.2.14/hotelinventoryapp/vite/deps_ssr/@angular_cdk_scrolling.js
@@ -0,0 +1,54 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-MSFKENNM.js";
+import "./chunk-BFMWBUBM.js";
+import "./chunk-JME5XKN5.js";
+import "./chunk-7233DXOT.js";
+import "./chunk-7OKZ4IVB.js";
+import {
+  Dir
+} from "./chunk-YKVLCPX5.js";
+import "./chunk-7VVESGAN.js";
+import "./chunk-62SVJMVC.js";
+import "./chunk-LEZCLC5X.js";
+import "./chunk-ZUJ64LXG.js";
+import "./chunk-XCIYP5SE.js";
+import "./chunk-OYTRG5F6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
